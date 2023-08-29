abcd620f70bc51c19056947a96ae26a129fbca08
Senaseser <seser0202@gmail.com>
2023-08-29 22:01:09 +0300

    layput created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Layout from "./components/Layout";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Layout>
+        <main className="min-h-[calc(100vh_-_68px)] mt-11">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </main>
+      </Layout>
     </div>
   );
 }
