47759df714c004a6db213b16d85d5b5b0f45ee43
Senaseser <seser0202@gmail.com>
2023-09-03 19:36:46 +0300

    Header and Hero Section is completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundColor: {
+      colors: {
         "brand-color": "#4C3398",
+        "primary-brand-color": "#5d3ebc",
+        "secondary-brand-color": "#7849f7",
+        "brand-yellow": "#FFD300",
+      },
+      backgroundImage: {
+        "hero-bg":
+          "linear-gradient( 90deg, var(--color-primary) 0%, rgba(93,62,188,0) 100% )",
       },
     },
   },
